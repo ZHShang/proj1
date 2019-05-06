80224d6026ca0e5eb2e4d2a4d9647f41a8596577
ZHShang <zhihengshang@hotmail.com>
2019-05-06 20:41:37 +0000

    acquired avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -5,13 +5,41 @@ console.log('Welcome to the GitHub Avatar Downloader');
 function getRepoContributors(repoOwner, repoName, cb){
   var options = {
     url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    header: {
-      'User-Agent': 'request'
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': token.GITHUB_TOKEN
     }
-};
+  }
   request(options, function(err, res, body){
-    cb(err, body);
+    cb(err,body);
   });
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result){
+    var data = JSON.parse(result);
+    data.forEach(function(user){
+      console.log(user.avatar_url);
+    })
+});
+
+
+/* var token = require('./secrets.js');
+var request = require('request');
+console.log('Welcome to the GitHub Avatar Downloader');
+
+function printBody(err, response, body){
+  console.log(body);
+}
+function getRepoContributors(repoOwner, repoName, cb){
+  var options = {
+    url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    header: {
+      'User-Agent': 'request',
+      'Authorization': token.GITHUB_TOKEN
+    }
+  };
+    request.get(options.url, printBody);
 
 }
 
@@ -20,3 +48,4 @@ getRepoContributors("jquery", "jquery", function(err, result){
   console.log("Result: ", result);
 });
 
+*/
